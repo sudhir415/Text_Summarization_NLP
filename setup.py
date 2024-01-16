b7801fc912b763441a6f438b9842fba21a76e78e
Sudhir Malik <sudhirmalik415@gmail.com>
2024-01-16 19:11:11 +0530

    Initial code setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer-Project"
+AUTHOR_USER_NAME = "entbappy"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
