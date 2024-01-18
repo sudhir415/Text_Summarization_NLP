f8d46bde6280507311293fae6932f2c794af12a7
Sudhir Malik <sudhirmalik415@gmail.com>
2024-01-19 04:01:34 +0530

    Data Transformation Commit

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -2,6 +2,7 @@ from src.textSummarizer.constants import *
 from src.textSummarizer.utils.common import read_yaml, create_directories 
 from src.textSummarizer.entity import DataIngestionConfig 
 from src.textSummarizer.entity import DataValidationConfig
+from src.textSummarizer.entity import DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -42,6 +43,19 @@ class ConfigurationManager:
             ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES
         )
 
-        return data_validation_config
+        return data_validation_config 
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation 
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig( 
+        root_dir = config.root_dir,
+        data_path = config.data_path,
+        tokenizer_name = config.tokenizer_name
+    )
+
+        return data_transformation_config 
     
 
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -13,4 +13,10 @@ class DataIngestionConfig:
 class DataValidationConfig:
     root_dir: Path
     STATUS_FILE: str
-    ALL_REQUIRED_FILES: list
+    ALL_REQUIRED_FILES: list 
+
+@dataclass(frozen=True) 
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: Path
