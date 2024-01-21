4d68d57a37a8b590b754dd7170984d2438ccb8eb
Sudhir Malik <sudhirmalik415@gmail.com>
2024-01-21 20:17:48 +0530

    Update Fast API APP

--- app.py
+++ app.py
@@ -8,38 +8,7 @@ from fastapi.responses import Response
 from textSummarizer.pipeline.prediction import PredictionPipeline 
 import logging
 from fastapi import Request
-
-
-# text:str = "What is Text Summarization?" 
-
-# app = FastAPI()
-
-# @app.get("/", tags=["authentication"])
-# async def index():
-#     return RedirectResponse(url="/docs")
-
-# templates = Jinja2Templates(directory="templates")
-# pipeline = PredictionPipeline()
-
-# @app.post("/predict", tags=["authentication"])
-# async def predict(text:str):
-#     summary = pipeline.predict(text)
-#     return templates.TemplateResponse("predict.html", {"request": Request, "summary": summary})
-
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-# @app.get("/train")
-# async def training():
-#     try:
-#         os.system("python main.py")
-#         return Response("Training successful !!")
-
-#     except Exception as e:
-#         return Response(f"Error Occurred! {e}")
-    
+  
 text:str = "What is Text Summarization?"
 
 app = FastAPI()
